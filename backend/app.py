dc873ec7f3e617708b991a74d7ed22456e6bef70
timapotiha99 <timapotiha99@gmail.com>
2025-06-20 21:07:08 +0000

    Add current progress for backend and frontend with docker-compose

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/api/health")
 def health():
     return {"status": "ok"}, 200
+    
+@app.route("/api/message")
+def message():
+    return {"message": "Привіт з Flask backend!"}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
